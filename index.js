42e78f4f438abcb9f785013c5d3b3fe6e6896773
ankitraj061 <ankitraj07032002@gmail.com>
2024-06-02 22:21:23 +0530

    added

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById("first-id").addEventListener("click",function(){
+    document.getElementById("first-id").innerHTML="Good Bye"
+    
+})
+
+document.querySelector("#second-id").addEventListener("click",function(){
+    document.querySelector("#second-id").innerHTML="Good Bye"
+    
+})
